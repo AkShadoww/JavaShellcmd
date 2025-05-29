01b2a1d0007668d7da0ef7c3799f52cc78687554
Anvith Krishna <152867087+AkShadoww@users.noreply.github.com>
2025-05-29 10:39:52 +0530

    Delete Bus_Ticket_Reservation directory

--- Bus_Ticket_Reservation/src/com/gqt/GUI/MainAppGUI.java
+++
@@ -1,9 +0,0 @@
-package com.gqt.GUI;
-
-import javax.swing.SwingUtilities;
-
-public class MainAppGUI {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LoginFrame());
-    }
-}
